e89315d8e33e314dfff188c2a449e3a9a2b1218a
SHYAM <SMUSANI@UCO.EDU>
2024-01-25 16:53:26 -0600

    init commit

---
+++ project02/project01-02_txt.js
@@ -0,0 +1,27 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: 
+    Date:   
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+const internetplans = {
+    Basic: {
+        service: "Basic",
+        DownloadSpeed:   "50 Mbps"
+    },
+    Express: {
+        service: "Express",
+        DownloadSpeed: "500 Mbps"
+        
+    },
+    ultimate: {
+        service: "ultimate",
+        DownloadSpeed: "1 Gig"
+
+    }
+}
