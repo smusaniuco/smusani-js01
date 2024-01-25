43422717c1eaadf4d5ec80e723d7af9386ad2e63
SHYAM <SMUSANI@UCO.EDU>
2024-01-25 17:22:26 -0600

    remove

--- project02/project01-02_txt.js
+++ project02/project01-02_txt.js
@@ -9,19 +9,3 @@
 */
 
 //define variables for service name and service speed
-const internetplans = {
-    Basic: {
-        service: "Basic",
-        DownloadSpeed:   "50 Mbps"
-    },
-    Express: {
-        service: "Express",
-        DownloadSpeed: "500 Mbps"
-        
-    },
-    ultimate: {
-        service: "ultimate",
-        DownloadSpeed: "1 Gig"
-
-    }
-}
