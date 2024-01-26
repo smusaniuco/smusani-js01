0dc63847b9a6c7ff0a1997bc82774acae9bb926e
SHYAM <SMUSANI@UCO.EDU>
2024-01-26 00:07:57 -0600

    change in js

--- project02/project01-02_txt.js
+++ project02/project01-02_txt.js
@@ -9,3 +9,11 @@
 */
 
 //define variables for service name and service speed
+var service1 = "Basic";
+var service2 = "Express";
+var service3 = "Extreme";
+var service4 = "Ultimate";
+var download1Speed = "50 Mbsp";
+var download2speed = "100 Mbsp";
+var download3speed = "500 Mbsp";
+var download4speed = "1 Gig";
